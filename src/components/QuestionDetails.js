f2bcd51f5a40503be587dd7e913fd6aafe1da463
KERFAH Issam <i.kerfah@gmail.com>
2021-10-02 23:10:11 +0300

    Fix: question not found

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -6,6 +6,9 @@ import Question from './Question';
 class QuestionsDetails extends Component {
     render() {
         const { id, isAnswered } = this.props
+        if (isAnswered == null) {
+            return <h3>Question with id {id} not found</h3>
+        }
         return (
             <div>
                 <h3 className='center'>Would You Rather</h3>
@@ -19,7 +22,8 @@ class QuestionsDetails extends Component {
 function mapStateToProps({ authedUser, questions }, props) {
     const { id } = props.match.params
     const question = questions[id]
-    const isAnswered = question.optionOne.votes.includes(authedUser.id) || question.optionTwo.votes.includes(authedUser.id)
+
+    const isAnswered = question ? (question.optionOne.votes.includes(authedUser.id) || question.optionTwo.votes.includes(authedUser.id)) : null
     return {
         id,
         isAnswered
